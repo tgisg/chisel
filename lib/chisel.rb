73345ee37676fc5e33e161f8db9b55fe06d6b175
tgisg <soniagupta504@gmail.com>
2016-04-21 21:35:13 -0600

    Worked on Headers.

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,4 +1,10 @@
+require 'pry'
 class Chisel
+  attr_accessor :markdown_array
+
+  def initialize(markdown_input) #markdown input must come in as one line only, so will need to parse chunks
+    @markdown_array = markdown_input.chars
+  end
 
   def read_file
     file = File.open("my_input.markdown", "r")
@@ -6,21 +12,43 @@ class Chisel
     text.chomp
   end
 
-  def convert_headers(markdown_input)
-    character_array = markdown_input.chars
-     if character_array[0..1] = '# '
-       character_array[0] = "<h1>"
-       character_array.push("</h1>")
+  def convert_headers
+    if markdown_array[0..1] == '# '.chars
+      markdown_array.delete_at(1)
+      markdown_array[0] = "<h1>"
+      markdown_array.push("</h1>")
 
-     elsif character_array[0..2] = '## '
-       character_array[0..1] = "<h2>"
-       character_array.push("</h2>")
-     end
-     character_array.join
-   end
+    elsif markdown_array[0..2] == '## '.chars
+      markdown_array.delete_at(2)
+      markdown_array[0..1] = "<h2>"
+      markdown_array.push("</h2>")
 
-    # markdown_input[0] = "<h1>"
-    # markdown_input + "</h1>"
+    elsif markdown_array[0..3] == '### '.chars
+      markdown_array.delete_at(3)
+      markdown_array[0..2] = "<h3>"
+      markdown_array.push("</h3>")
+
+    elsif markdown_array[0..4] == '#### '.chars
+      markdown_array.delete_at(4)
+      markdown_array[0..3] = "<h4>"
+      markdown_array.push("</h4>")
+
+    elsif markdown_array[0..5] == '##### '.chars
+      markdown_array.delete_at(5)
+      markdown_array[0..4] = "<h5>"
+      markdown_array.push("</h5>")
+
+    elsif markdown_array[0] != '#'.chars
+      # binding.pry
+      markdown_array.unshift("<p>")
+      markdown_array.push("</p>")
+    end
+    markdown_array.join
+
+  end
+
+  # markdown_input[0] = "<h1>"
+  # markdown_input + "</h1>"
 end
 
 
@@ -40,7 +68,7 @@ end
 
 # =>
 
-  #part one - finish the hash
+#part one - finish the hash
 #   def html_symbol
 #     hash_of_symbols = {'#' => ["<h1>", "</h1>"],
 #                       '##' => ["<h2>", "</h2>"],
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -2,26 +2,49 @@ gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/chisel'
+require 'pry'
 
 class ChiselTest < Minitest::Test
 
   # def test_can_convert_single_markdown_header_to_html_header
   #   chisel = Chisel.new
   #
-  #   assert_equal('# hello', chisel.convert_headers('# hello'))
+  #   assert_equal('# hello'.convert_headers('# hello'))
   # end
 
   # <h1>hello</h1>
-  def test_can_convert_single_markdown_header_to_html_header_1
-    chisel = Chisel.new
-    result = chisel.convert_headers('# hello')
-    assert_equal('<h1>hello</h1>', result)
+  def test_can_split_string_into_array
+    chisel = Chisel.new("# hello")
+    assert_equal(['#', ' ', 'h', 'e', 'l', 'l', 'o'], chisel.markdown_array)
   end
 
-  def test_can_convert_double_markdown_header_to_html_header_2
-    chisel = Chisel.new
-    result = chisel.convert_headers('## hello')
-    assert_equal('<h2>hello</h2>', result)
+  def test_can_convert_one_markdown_header_to_html_header_1
+    chisel = Chisel.new('# hello')
+    assert_equal('<h1>hello</h1>', chisel.convert_headers)
   end
 
+  def test_can_convert_two_markdown_header_to_html_header_2
+    chisel = Chisel.new('## hello')
+    assert_equal('<h2>hello</h2>', chisel.convert_headers)
+  end
+
+  def test_can_convert_three_markdown_header_to_html_header_3
+    chisel = Chisel.new('### hello')
+    assert_equal('<h3>hello</h3>', chisel.convert_headers)
+  end
+
+  def test_can_convert_four_markdown_header_to_html_header_4
+    chisel = Chisel.new('#### hello')
+    assert_equal('<h4>hello</h4>', chisel.convert_headers)
+  end
+
+  def test_can_convert_five_markdown_header_to_html_header_5
+    chisel = Chisel.new('##### hello')
+    assert_equal('<h5>hello</h5>', chisel.convert_headers)
+  end
+
+  def test_can_convert_no_markdown_header_to_html_para
+    chisel = Chisel.new("hello")
+    assert_equal('<p>hello</p>', chisel.convert_headers)
+  end
 end
